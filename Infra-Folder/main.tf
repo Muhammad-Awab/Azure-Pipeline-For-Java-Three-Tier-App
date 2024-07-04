7007f3b3cef89f3001fc1149e6e56b7b2fc41017
Muhammad-Awab <muhammadawab82002@gmail.com>
2024-07-04 23:10:59 +0500

    Add Azure Pipeline File

---
+++ Infra-Folder/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+  version         = ">= 3.0.0"
+  skip_provider_registration = true
+}
+
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "ForzaCapstoneAKSCluster-java"
+  location            = "East US"
+  resource_group_name = "ForzaCapstoneResourceGroup"
+  dns_prefix          = "forzaaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
